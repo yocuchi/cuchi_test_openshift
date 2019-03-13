621ee748a183f63a73f61ea3115f71ff1fd02f9a
Tu Nombre <you@example.com>
2019-03-13 09:29:54 +0100

    dsfsdf

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 echo "<h1> PROBANDO </h1>";
-echo "SERVER" . $_SERVER['SERVER_ADDR'] . "<br/>";
-echo  $_SERVER['HTTP_HOST'] ;
-echo $_ENV['PHP1_PORT'];
+echo "<h2>SERVER" . $_SERVER['SERVER_ADDR'] . "</h2><br/>";
+//echo  $_SERVER['HTTP_HOST'] ;
+//echo $_ENV['PHP1_PORT'];
 
-var_dump($_SERVER);
+//var_dump($_SERVER);
 //phpinfo();
 ?>
